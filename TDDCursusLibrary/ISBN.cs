dbdd175a6c729ac31485ec1291edddb590c8a7fd
MichielAchten <michiel.achten@gmail.com>
2016-05-20 15:57:23 +0200

    Commit2

---
+++ TDDCursusLibrary/ISBN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCursusLibrary
+{
+    public class Isbn
+    {
+        public Isbn(long nummer)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDDCursusLibraryTest/ISBNTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDCursusLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDCursusLibraryTest
+{
+    [TestClass]
+    public class ISBNTest
+    {
+        [TestMethod]
+        public void NummerMet13CijfersEnEenJuistControleGetal2()
+        {
+            new Isbn(9789027439642L);    //dit nummer mag geen exception veroorzaken
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void NummerMet13CijfersEnEenVerkeerdControleGetal2()
+        {
+            new Isbn(8789027439642L);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void NummerMet13CijfersEnEenFoutControleGetal0()
+        {
+            new Isbn(7789027439640L);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void EenNegatiefNummerIsVerkeerd()
+        {
+            new Isbn(-9789027439642L);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void NummerMet14CijfersIsVerkeerd()
+        {
+            new Isbn(97890274396421L);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void NummerMet12CijfersIsVerkeerd()
+        {
+            new Isbn(978902743964L);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void HetNummer0IsVerkeerd()
+        {
+            new Isbn(0);
+        }
+
+    }
+}
