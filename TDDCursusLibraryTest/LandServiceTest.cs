b1242eb79479f9c88a8dde409decdd8891ab31d8
MichielAchten <michiel.achten@gmail.com>
2016-05-23 12:18:24 +0200

    Oefening Mock

--- TDDCursusLibraryTest/LandServiceTest.cs
+++ TDDCursusLibraryTest/LandServiceTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TDDCursusLibrary;
+using Moq;
 
 namespace TDDCursusLibraryTest
 {
@@ -13,16 +14,23 @@ namespace TDDCursusLibraryTest
     {
         private ILandDAO landDAO;
         private LandService landService;
+        private Mock<ILandDAO> mockFactory;
         [TestInitialize]
         public void Initialize()
         {
-            landDAO = new LandDAOStub();
+            mockFactory = new Mock<ILandDAO>();
+            landDAO = mockFactory.Object;
+            //landDAO = new LandDAOStub();
+            mockFactory.Setup(eenLandDAO => eenLandDAO.OppervlakteAlleLanden()).Returns(20);
+            mockFactory.Setup(eenLandDAO => eenLandDAO.Read("B")).Returns(new Land { Landcode = "B", Oppervlakte = 5 });
             landService = new LandService(landDAO);
         }
         [TestMethod]
         public void VerhoudingOppervlakteLandTovAlleLanden()
         {
             Assert.AreEqual(0.25m, landService.VerhoudingOppervlakteLandTovOppervlakteAlleLanden("B"));
+            mockFactory.Verify(eenLandDAO => eenLandDAO.OppervlakteAlleLanden());
+            mockFactory.Verify(eenLandDAO => eenLandDAO.Read("B"));
         }
     }
 }
--- TDDCursusLibraryTest/WinstServiceTest.cs
+++ TDDCursusLibraryTest/WinstServiceTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TDDCursusLibrary;
+using Moq;
 
 namespace TDDCursusLibraryTest
 {
@@ -14,17 +15,27 @@ namespace TDDCursusLibraryTest
         private WinstService winstService;
         private IOpbrengstDAO opbrengstDAO;
         private IKostDAO kostDAO;
+        private Mock<IKostDAO> mockKostDAO;
+        private Mock<IOpbrengstDAO> mockOpbrengstDAO;
         [TestInitialize]
         public void Initialize()
         {
-            opbrengstDAO = new OpbrengstDAOStub();
-            kostDAO = new KostDAOStub();
+            mockKostDAO = new Mock<IKostDAO>();
+            mockOpbrengstDAO = new Mock<IOpbrengstDAO>();
+            kostDAO = mockKostDAO.Object;
+            opbrengstDAO = mockOpbrengstDAO.Object;
+            //opbrengstDAO = new OpbrengstDAOStub();
+            //kostDAO = new KostDAOStub();
+            mockKostDAO.Setup(eenKostDAO => eenKostDAO.FindTotaleKost()).Returns(169m);
+            mockOpbrengstDAO.Setup(eenOpbrengstDAO => eenOpbrengstDAO.FindTotaleOpbrengst()).Returns(200m);
             winstService = new WinstService(opbrengstDAO, kostDAO);
         }
         [TestMethod]
         public void WinstIsOpbrengstMinKost()
         {
             Assert.AreEqual(31m, winstService.Winst);
+            mockKostDAO.Verify(eenKostDAO => eenKostDAO.FindTotaleKost());
+            mockOpbrengstDAO.Verify(eenOpbrengstDAO => eenOpbrengstDAO.FindTotaleOpbrengst());
         }
     }
 }
