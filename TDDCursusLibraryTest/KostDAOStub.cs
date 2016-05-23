890eaa21c9fdcabffae4ce8be9281d6cb15a602e
MichielAchten <michiel.achten@gmail.com>
2016-05-23 11:23:52 +0200

    Stub oefening

---
+++ TDDCursusLibraryTest/KostDAOStub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDCursusLibrary;
+
+namespace TDDCursusLibraryTest
+{
+    public class KostDAOStub: IKostDAO
+    {
+        public decimal FindTotaleKost()
+        {
+            return 169m;
+        }
+
+    }
+}
---
+++ TDDCursusLibraryTest/WinstServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDCursusLibrary;
+
+namespace TDDCursusLibraryTest
+{
+    [TestClass]
+    public class WinstServiceTest
+    {
+        private WinstService winstService;
+        private IOpbrengstDAO opbrengstDAO;
+        private IKostDAO kostDAO;
+        [TestInitialize]
+        public void Initialize()
+        {
+            opbrengstDAO = new OpbrengstDAOStub();
+            kostDAO = new KostDAOStub();
+            winstService = new WinstService(opbrengstDAO, kostDAO);
+        }
+        [TestMethod]
+        public void WinstIsOpbrengstMinKost()
+        {
+            Assert.AreEqual(31m, winstService.Winst);
+        }
+    }
+}
